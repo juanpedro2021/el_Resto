1651d3c390f378cca7436e697ea30bf5105c1589
gianfpino <gianfpino@gmail.com>
2023-10-03 17:43:29 -0300

    prueba para ObtenerProducto

--- src/Principal/ElResto.java
+++ src/Principal/ElResto.java
@@ -13,7 +13,17 @@ public class ElResto {
        
 //       Conexion con= new Conexion();
 //       con= (Conexion) Conexion.getConexion();
-       
+       //////PROBAR OBTENER PRODUCTO/////
+//      ProductoData pd=new ProductoData();
+//      List<Producto> lp= pd.obtenerProducto();
+//      
+//      for(Producto produ : lp){
+//          System.out.println("ID: " + produ.getidProducto());
+//          System.out.println("Codigo: " + produ.getCodigo());
+//          System.out.println("Nombre: " + produ.getNombre());
+//          System.out.println("Cantidad: " + produ.getCantidad());
+//          System.out.println("Precio: " + produ.getPrecio());
+//      }
       
     }
     
