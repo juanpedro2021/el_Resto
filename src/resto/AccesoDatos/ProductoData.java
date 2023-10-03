4ada3efd3aa5bf6300c1b93cd6f3706e0d9675ca
pableKo <141676090+pableKo@users.noreply.github.com>
2023-10-03 17:09:56 -0300

    cree EliminarProducto en Acceso

--- src/resto/AccesoDatos/ProductoData.java
+++ src/resto/AccesoDatos/ProductoData.java
@@ -2,6 +2,9 @@
 package resto.AccesoDatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 
 public class ProductoData {
@@ -11,6 +14,22 @@ public class ProductoData {
         con= Conexion.getConexion();
     }
     
+    public void eliminarProducto(int id) {
+String sql = "UPDATE producto SET estado = 0 WHERE idProducto = ? ";
+	try {
+		
+		PreparedStatement ps = con.prepareStatement (sql);
+		ps.setInt(1, id);
+		int fila=ps.executeUpdate();
+
+		if (fila==1) {
+			JOptionPane.showMessageDialog(null, "Se eliminó el producto");
+		}
+		ps.close();
+		} catch (SQLException ex) {
+		JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Producto "+ ex.getMessage());
+		}
+	}
     
     
 }
