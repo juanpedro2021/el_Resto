8ee00aba9fa9157996d28e07d5d962e9db372c3a
juanpedro2021 <juanpedro103.jp@gmail.com>
2023-10-03 16:20:42 -0300

    creando Mesa

--- src/resto/Entidades/Mesa.java
+++ src/resto/Entidades/Mesa.java
@@ -1,13 +1,66 @@
-/*Puto el que leePuto el que leePuto el que leePuto el que leePuto el que leePuto el que leePuto el que lee*/
+
 package resto.Entidades;
 
-/**
- *
- * @author lau-d
- */
+
 public class Mesa {
-//        comentario
-public void prueba1(){
+    private int idMesa;
+    private int numMesa;
+    private int capacidad;
+    private int estado ;
+
+    public Mesa(int idMesa, int numMesa, int capacidad, int estado) {
+        this.idMesa = idMesa;
+        this.numMesa = numMesa;
+        this.capacidad = capacidad;
+        this.estado = estado;
+    }
+
+    public Mesa(int numMesa, int capacidad, int estado) {
+        this.numMesa = numMesa;
+        this.capacidad = capacidad;
+        this.estado = estado;
+    }
+
+    public Mesa() {
+    }
+
+    public int getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(int idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public int getNumMesa() {
+        return numMesa;
+    }
+
+    public void setNumMesa(int numMesa) {
+        this.numMesa = numMesa;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesa{" + "idMesa=" + idMesa + ", numMesa=" + numMesa + ", capacidad=" + capacidad + ", estado=" + estado + '}';
+    }
     
-}    
+    
+
 }
