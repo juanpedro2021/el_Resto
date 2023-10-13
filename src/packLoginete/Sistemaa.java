31d6928d45ad6aa9d2043b7c561e7c52a2f90536
pableKo <141676090+pableKo@users.noreply.github.com>
2023-10-13 17:15:41 -0300

    cree pack Loginete
    en otro pack están las imagenes que baje

---
+++ src/packLoginete/Sistemaa.java
@@ -0,0 +1,11 @@
+
+package packLoginete;
+
+
+class Sistemaa {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
